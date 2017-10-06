e0ddaa9f05576ba5cd35977e65815d7089e1ba3b
Alex Hannigan <alexhannigan@AlexnoMacBook-Air.local>
2017-10-06 17:10:30 +0100

    Initial Commit

---
+++ CalcHard/CalculatorBrain.swift
@@ -0,0 +1,148 @@
+//
+//  CalculatorBrain.swift
+//  CalcHard
+//
+//  Created by Alex Hannigan on 2017/10/01.
+//  Copyright © 2017年 Alex Hannigan. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+    private var resetEverythingOnNextOperandReceived = true
+    
+    // The result of our calculation so far.
+    private var accumulator: Double?
+    
+    // The sequence of operands and operations used in our calculation so far.
+    private var description = ""
+    
+    // Returns whether we are currently in the middle of a binary operation.
+    var resultIsPending: Bool {
+        return pendingBinaryOperation != nil
+    }
+    
+    // All of the generic types of operation that can be performed.
+    private enum Operation {
+        case constant(Double)
+        case unaryOperation((Double) -> Double)
+        case binaryOperation((Double,Double) -> Double)
+        case equals
+        case resetEverything
+    }
+    
+    // All of the specific operations that can be performed (each is one of the above generic types).
+    private var operations = [
+        "π": Operation.constant(Double.pi),
+        "e": Operation.constant(M_E),
+        "√": Operation.unaryOperation(sqrt),
+        "±": Operation.unaryOperation({-$0}),
+        "x²": Operation.unaryOperation({$0 * $0}),
+        "x³": Operation.unaryOperation({$0 * $0 * $0}),
+        "sin": Operation.unaryOperation(sin),
+        "cos": Operation.unaryOperation(cos),
+        "tan": Operation.unaryOperation(tan),
+        "+": Operation.binaryOperation(+),
+        "−": Operation.binaryOperation(-),
+        "×": Operation.binaryOperation(*),
+        "÷": Operation.binaryOperation(/),
+        "=": Operation.equals,
+        "C": Operation.resetEverything
+    ]
+    
+    // Stores the first operand in our accumulator, and appends it to our description.
+    mutating func setOperand(to operand: Double) {
+        accumulator = operand
+        if resultIsPending {
+            description += String(operand)
+        }
+        else if description == "" {
+            description = String(operand)
+        }
+    }
+    
+    // Perform the appropriate operation according to the mathematical symbol received.
+    mutating func performOperation(_ mathematicalSymbol:String) {
+        if let symbol = operations[mathematicalSymbol] {
+            switch symbol {
+            case .constant(let value):
+                accumulator = value
+                description = mathematicalSymbol
+            case .unaryOperation(let function):
+                if accumulator != nil {
+                    if resultIsPending {
+                        switch mathematicalSymbol {
+                        case "x²":
+                            description += "(\(accumulator!))²"
+                        case "x³":
+                            description += "(\(accumulator!))³"
+                        default:
+                            description += "\(mathematicalSymbol)(\(accumulator!))"
+                        }
+                        accumulator = function(accumulator!)
+                        performPendingBinaryOperation()
+                    }
+                    else {
+                        switch mathematicalSymbol {
+                        case "x²":
+                            description = "(\(description))²"
+                        case "x³":
+                            description = "(\(description))³"
+                        default:
+                            description = "\(mathematicalSymbol)(\(description))"
+                        }
+                        accumulator = function(accumulator!)
+                    }
+                }
+            case .binaryOperation(let function):
+                if accumulator != nil {
+                    if pendingBinaryOperation != nil {
+                        performPendingBinaryOperation()
+                    }
+                    pendingBinaryOperation = PendingBinaryOperation(firstOperand: accumulator!, function: function)
+                    accumulator = nil
+                    description += mathematicalSymbol
+                }
+            case .equals:
+                if accumulator != nil && pendingBinaryOperation != nil {
+                    performPendingBinaryOperation()
+                    pendingBinaryOperation = nil
+                }
+            case .resetEverything:
+                accumulator = nil
+                description = ""
+                pendingBinaryOperation = nil
+            }
+        }
+    }
+    
+    // If we are currently in the middle of a binary operation, this var will hold the first operand
+    // and the appropriate function to be performed.
+    // If not, this will be 'nil'.
+    private var pendingBinaryOperation: PendingBinaryOperation?
+    
+    private struct PendingBinaryOperation {
+        let firstOperand: Double
+        let function: (Double,Double) -> Double
+        
+        func perform(with secondOperand:Double) -> Double {
+            return function(firstOperand, secondOperand)
+        }
+    }
+    
+    // Complete our pending binary operation and store the result in our accumulator.
+    mutating private func performPendingBinaryOperation() {
+        accumulator = pendingBinaryOperation?.perform(with: accumulator!)
+    }
+    
+    // A read-only var which returns the result of our calculation so far.
+    var result:Double? {
+        return accumulator
+    }
+    
+    // A read-only var which returns the sequence of operands and operations used in our calculation so far.
+    var getDescription: String {
+        return description
+    }
+}
---
+++ CalcHard/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  CalcHard
+//
+//  Created by Alex Hannigan on 2017/10/01.
+//  Copyright © 2017年 Alex Hannigan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // This label displays the result of our calculation so far.
+    @IBOutlet weak var calculatorScreen: UILabel!
+    
+    //This label displays the sequence of operands and operations used in our calculation so far.
+    @IBOutlet weak var descriptionScreen: UILabel!
+    
+    // Returns 'true' if the next digit typed by the user should be appended
+    // to the digits currently in the display, and 'false' if it should
+    // replace them.
+    private var appendTheNextDigit = false
+    private var calculatorBrain = CalculatorBrain()
+    
+    // Sets and gets the value currently on the 'result' screen of our calculator,
+    // converting to a String (from Int) to set, and converting to an Int (from String) to get.
+    private var displayValue:Double {
+        get {
+            return Double(calculatorScreen.text!)!
+        }
+        set {
+            calculatorScreen.text = String(newValue)
+        }
+    }
+    
+    // Sets the 'result' screen to the number currently being typed by the user.
+    @IBAction func userInputToDisplay(_ sender: UIButton) {
+        // Append the digit to the digits currently in the display.
+        if appendTheNextDigit {
+            // Prevents multiple decimal points from being input.
+            if sender.currentTitle! != "." || !calculatorScreen.text!.contains(".") {
+                calculatorScreen.text = calculatorScreen.text! + sender.currentTitle!
+            }
+        }
+        // Replace the digits currently in the display.
+        else {
+            if sender.currentTitle! == "." {
+                calculatorScreen.text = "0\(sender.currentTitle!)"
+            }
+            else {
+                calculatorScreen.text = sender.currentTitle!
+            }
+            appendTheNextDigit = true
+        }
+    }
+    
+    // Undo the last input of a digit by the user.
+    @IBAction func undoLastDigitPress() {
+        if appendTheNextDigit {
+            if calculatorScreen.text!.characters.count > 1 {
+                // Remove it from the string of digits in the screen or..
+                calculatorScreen.text! = String(calculatorScreen.text!.characters.dropLast())
+            }
+            else {
+                // ...reset the screen to '0'.
+                calculatorScreen.text! = "0"
+                appendTheNextDigit = false
+            }
+        }
+    }
+    
+    // Perform the operation selected by the user,
+    // using the value currently on the calculator screen.
+    // If the operation was binary, get ready to accept the second operand.
+    // Otherwise, display the result on the screen.
+    @IBAction func performOperation(_ sender: UIButton) {
+        calculatorBrain.setOperand(to: displayValue)
+        calculatorBrain.performOperation(sender.currentTitle!)
+        if let result = calculatorBrain.result {
+            displayValue = result
+            descriptionScreen.text = calculatorBrain.getDescription + "="
+        }
+        else if calculatorBrain.resultIsPending {
+            descriptionScreen.text = calculatorBrain.getDescription + "..."
+        }
+        else {
+            calculatorScreen.text = "0"
+            descriptionScreen.text = "0"
+        }
+        appendTheNextDigit = false
+    }
+    
+}
+
